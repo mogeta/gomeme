37e6b46c3844e60d7776fbb2066ce247f8d0ad73
mogeta <muzigen@gmail.com>
2018-11-30 18:19:11 +0900

    split config client token

--- gomeme.go
+++ gomeme.go
@@ -2,44 +2,71 @@ package gomeme
 
 import (
 	"context"
-	"github.com/davecgh/go-spew/spew"
+	"fmt"
 	"golang.org/x/oauth2"
 	"log"
+	"net/http"
+	"net/url"
+	"time"
 )
 
-type Client struct {
+var endpoint = oauth2.Endpoint{
+	AuthURL:  "https://accounts.jins.com/jp/ja/oauth/authorize",
+	TokenURL: "https://apis.jins.com/meme/v1/oauth/token",
+}
+
+type Gomeme struct {
 	OAuth2Config *oauth2.Config
 }
 
-func NewClient(clientID string, clientSecret string, redirectURL string, scopes []string) Client {
-	oauth2.RegisterBrokenAuthHeaderProvider("https://apis.jins.com/meme/v1/oauth/token")
-	return Client{
+type memeClient struct {
+	Client *http.Client
+	Token  *oauth2.Token
+}
+
+func NewConfig(clientID string, clientSecret string, redirectURL string, scopes []string) Gomeme {
+	oauth2.RegisterBrokenAuthHeaderProvider(endpoint.TokenURL)
+	return Gomeme{
 		OAuth2Config: &oauth2.Config{
 			RedirectURL:  redirectURL,
 			ClientID:     clientID,
 			ClientSecret: clientSecret,
 			Scopes:       scopes,
-			Endpoint: oauth2.Endpoint{
-				AuthURL:  "https://accounts.jins.com/jp/ja/oauth/authorize",
-				TokenURL: "https://apis.jins.com/meme/v1/oauth/token",
-			},
+			Endpoint:     endpoint,
 		},
 	}
 
 }
 
-func (g Client) Exchange(authCode string) {
+func (g Gomeme) Exchange(authCode string) *oauth2.Token {
 
-	token, err := g.OAuth2Config.Exchange(context.Background(), authCode)
+	token, err := g.OAuth2Config.Exchange(context.TODO(), authCode)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	spew.Dump(token)
-	//client := conf.Client(context.Background(), token)
+	return token
 }
 
-func (g Client) GetAuthCode() string {
+func (g Gomeme) GetAuthCodeURL() string {
 	url := g.OAuth2Config.AuthCodeURL("state", oauth2.SetAuthURLParam("service_id", "meme"))
 	return url
+}
+
+func NewClient(meme Gomeme, token *oauth2.Token) memeClient {
+	return memeClient{
+		Client: meme.OAuth2Config.Client(context.TODO(), token),
+		Token:  token,
+	}
+}
+
+func (c memeClient) GetMeasutreData(from, to time.Time, cursor string) {
+	v := url.Values{}
+	v.Add("access_token", c.Token.AccessToken)
+	v.Add("action", "getactivity")
+
+	path := fmt.Sprintf("%s?%s", "https://apis.jins.com/meme/v1/users/me/office2/computed_data", v.Encode())
+	req, _ := http.NewRequest("GET", path, nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Token.AccessToken))
+	req.Header.Set("Accept", "application/json")
+
 }
--- tools/main.go
+++ tools/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
 	"gomeme"
+	"log"
 	"net/http"
+	"os"
 )
 
-var client gomeme.Client
+var meme gomeme.Gomeme
 
 func main() {
 	initConfig("config")
@@ -17,19 +20,17 @@ func main() {
 	RedirectURL := viper.GetString("Jins.RedirectURL")
 	scopes := viper.GetStringSlice("Jins.Scopes")
 
-	client = gomeme.NewClient(clientID, clientSecret, RedirectURL, scopes)
-	spew.Dump(client)
-	url := client.GetAuthCode()
+	meme = gomeme.NewConfig(clientID, clientSecret, RedirectURL, scopes)
 
-	fmt.Println(url)
-
-	http.HandleFunc("/", handler)
-	go http.ListenAndServe(":8080", nil)
-
-	var s string
-	fmt.Scan(&s)
-	fmt.Println(s)
-
+	token, err := tokenFromFile("token.json")
+	if err != nil {
+		url := meme.GetAuthCodeURL()
+		fmt.Println(url)
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(":8080", nil)
+	}
+	client := gomeme.NewClient(meme, token)
+	fmt.Println(client)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -40,13 +41,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	code := r.Form.Get("code")
 	fmt.Println(code)
-	spew.Dump(client)
-	client.Exchange(code)
+	token := meme.Exchange(code)
+	saveToken("token.json", token)
+
 	fmt.Fprintf(w, "Hello, World")
 }
 
-func getAuthTokenURL() {
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
 
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func initConfig(filename string) {
